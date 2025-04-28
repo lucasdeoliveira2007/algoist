e25edb755a708bbca9b5cdb8d1832f97b4247e33
lucasdeoliveira2007 <lucasdesaoliveira123@gmail.com>
2025-04-28 15:20:33 -0400

    Add files via upload

---
+++ exemplo_funcao.py
@@ -0,0 +1,35 @@
+#print("tabuada do 7")
+#for i in range(1,11):
+    #print("7 x ", 1,"= ",7 * i)
+   
+#print("tabuada do 8")
+#for i in range(1,11):
+    #print("8 x ", 1,"= ",8 * i)
+
+#print("tabuada do 9")
+#for i in range(1,11):
+  #print("9 x ", 1,"= ",9 * i)
+
+#def tabuada(numero): 
+  #print(f"tabuada do(numero):")
+#for 1 in range(1,11):
+  #print(f"(numero) x (1) =(numero *1 )")
+
+  #tabuada (7)
+  #tabuada (8)
+  #tabuada (9)
+
+#def tabuada(numero):
+  #print(f'tabuada do {numero}:')
+  #for 1 in range(1,11):
+   # print(f'{numero} x {1} ={numero *i }')
+    #print()
+#for i in range (1,101):
+  # tabuada(1)
+
+#def tabuada_personalizada (base, inicio, fim):
+   # print(f"tabuada do {base} de {inicio} e {fim}: ")
+    #for j in range (inicio, fim +1):
+        #print(f'{base} x {j} = {base * j }')
+    #tabuada_personalizada ( 7, 1, 10)
+    #tabuada_personalizada (5,5,15)
